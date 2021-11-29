92233ffe6e1d708a4d082b0f32da1fa3c1d437af
Aidan Hughes <aidanhughes294@yahoo.com>
2021-11-29 18:25:19 +0000

    Added media tag to prize winners, linked to their ID number

--- make_dataset.py
+++ make_dataset.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://127.0.0.1:27017")
 db = client.project
 laureates = db.laureates
+prizes = db.prizes
 
 def add_tributes_to_laureates():
     for laureate in laureates.find():
@@ -39,9 +40,25 @@ def add_media_tag_to_laureate():
         id = laureate["id"]
         laureates.update_one(
             { "_id" : laureate["_id"]},
-            { "$set" : { "profileImage" : "assets/images/" + id + ".jpg"} }
+            { "$set" : { "profileImage" : "assets/images/" + id + ".jpg" } }
         )
+
+
+def add_media_tag_to_prize():
+    for prize in prizes.find():
+        try:
+            for laureate in prize["laureates"]:
+                id = laureate["id"]
+                prizes.update_one(
+                    { "laureates.id" : id },
+                    { "$set" : { "laureates.$.profileImage" : "assets/images/" + id + ".jpg" } }
+                )
+        except:
+            # No prize exists for this year, skipping iteration.
+            continue
+
 
 # add_tributes_to_laureates()
 # add_object_id_to_prizes_in_laureates_collection()
 # add_media_tag_to_laureate()
+# add_media_tag_to_prize()
